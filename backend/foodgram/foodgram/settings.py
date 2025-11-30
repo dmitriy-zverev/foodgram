3d59e3d408dd0fde8342add2ba7f7ed75d38752b
dmitriy-zverev <dimazverev716@gmail.com>
2025-11-30 18:40:47 +0300

    update admin panel, add docker for production

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-*zc___aubizri6e&n2695^c%rg)oy&_ogo%o4krgg^z-s$i=c+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'backend']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'backend', '82.202.141.103']
 
 # Application definition
 
